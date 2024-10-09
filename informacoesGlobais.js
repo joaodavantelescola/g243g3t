fbdb316a930ef348200a38704ae07109341674a4
joaodavantelescola <110620034+joaodavantelescola@users.noreply.github.com>
2024-10-09 08:49:05 -0300

    .

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -2,6 +2,17 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 
 async function buscarinformacoes() {
     const res = await fetch(url);
-    const dados = res.json();
-    console.log(dados);
+    const dados = await res.json();
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+
+    const pessoasConectadas = dados.total_pessoas_conectadas / le9;
+    const pessoasNoMundo = dados.total_pessoas_mundo / le9;
+    const horas = parseInt(dados.tempo_médio);
+    const minutos = Math.round((dados.tempo_médio - horas))*60;
+    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo)*100).toFixed(2);
+
+    paragrafo.innerHTML = `Voce sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente<span>${dados.total_pessoas_conectadas}bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_médio}</span> horas conectadas.`;
+     document.getElementById('graficos-container').appendChild(paragrafo);
 }
+buscarinformacoes();
